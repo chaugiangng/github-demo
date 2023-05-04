e4d19a9e41f693b462688569a3bc11f5c4b8685b
chaugiangng <chaugiangng2222@gmail.com>
2023-05-04 14:27:14 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Giang Giang diu diu"/>
+    <HelloWorld msg="Giang Giang"/>
   </div>
 </template>
 
